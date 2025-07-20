6cf792ba0cd146108528e88fd9fa3e39d32df8a0
Stevethedev212 <125455527+Stevethedev212@users.noreply.github.com>
2025-07-20 19:05:44 +0000

    class work

---
+++ python_introduction/matchdata.py
@@ -0,0 +1,13 @@
+
+
+
+age = int(input("Enter your age: "))
+
+match age:
+    case 18 | 19:  # Match multiple values with pipe (|)
+        if age >= 18: # Guard using a function call
+            print("You are eligible to vote.")
+        else:
+            print("You need a valid ID to vote.")
+    case _:
+        print("You are not yet eligible to vote.")
